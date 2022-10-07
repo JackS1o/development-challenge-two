3a47274e525989b86a5c6c300551341f968f1712
Jackson Viana <jviana116@gmail.com>
2022-10-06 23:19:07 -0300

    feat: adicionando css

--- front-end/src/pages/GetPatients.jsx
+++ front-end/src/pages/GetPatients.jsx
@@ -2,6 +2,22 @@ import React from "react";
 import { getAllPatients } from "../apis/api";
 import Button from "@mui/material/Button";
 import Stack from "@mui/material/Stack";
+import Box from "@mui/material/Box";
+import CssBaseline from "@mui/material/CssBaseline";
+import Container from "@mui/material/Container";
+import "../index.css";
+
+const mapStyle = {
+  border: "1px solid black",
+  width: "500px",
+  height: "200px",
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+  alignItems: "center",
+  borderRadius: "16px",
+  bgcolor: "#9cee8bad",
+};
 
 export default function GetPatients() {
   const [patients, setPatients] = React.useState([]);
@@ -12,28 +28,47 @@ export default function GetPatients() {
     const patient = await getAllPatients();
     setLoading(false);
     setPatients(patient);
-    console.log(patient);
   };
 
   return (
-    <div className="container">
-      <h1>Get Patients</h1>
-      <Stack direction="row" spacing={2}>
-        {loading ? (
-          <Button variant="contained">
-            Loading
-          </Button>
-        ) : (
-          <Button variant="contained" onClick={getPatients}>
-            Get Patients
-          </Button>
-        )}
-      </Stack>
-      <div>
-        {patients.map((patient, index) => (
-          <h1 key={index}>{patient.patient_name}</h1>
-        ))}
-      </div>
-    </div>
+    <>
+      <CssBaseline />
+      <Container
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          flexDirection: "column",
+        }}
+      >
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            gap: "10px",
+          }}
+        >
+          <h1>Get Patients</h1>
+          <Stack direction="row" spacing={2}>
+            {loading ? (
+              <Button variant="contained">Loading</Button>
+            ) : (
+              <Button variant="contained" onClick={getPatients}>
+                Get Patients
+              </Button>
+            )}
+          </Stack>
+          {patients.map((patient, index) => (
+            <Box sx={{ ...mapStyle }}>
+              <p key={index}><strong>Name: </strong>{patient.patient_name}</p>
+              <p key={index}><strong>Birth Date: </strong>{patient.birth_date}</p>
+              <p key={index}><strong>Email: </strong>{patient.email}</p>
+              <p key={index}><strong>Address: </strong>{patient.patient_address}</p>
+            </Box>
+          ))}
+        </Box>
+      </Container>
+    </>
   );
 }
--- front-end/src/pages/Home.jsx
+++ front-end/src/pages/Home.jsx
@@ -2,6 +2,20 @@ import React from "react";
 import CssBaseline from "@mui/material/CssBaseline";
 import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
+import { Link } from "react-router-dom";
+import "../index.css";
+
+const boxStyle = {
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  borderRadius: "16px",
+  border: "1px solid black",
+  padding: "20px",
+  width: "400px",
+  color: "#000000",
+  height: "100px",
+};
 
 export default function Home() {
   return (
@@ -19,62 +33,81 @@ export default function Home() {
             flexWrap: "wrap",
           }}
         >
-          <Box
-            sx={{
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-              borderRadius: "16px",
-              border: "1px solid black",
-              bgcolor: "#11a0d4",
-              padding: "20px",
-              width: "400px",
-            }}
-          >
-            <h1>Create Patient</h1>
-          </Box>
-          <Box
-            sx={{
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-              borderRadius: "16px",
-              border: "1px solid black",
-              bgcolor: "#1ac94c",
-              padding: "20px",
-              width: "400px",
-            }}
-          >
-            <h1>Get All Patients</h1>
-          </Box>
-          <Box
-            sx={{
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-              borderRadius: "16px",
-              border: "1px solid black",
-              bgcolor: "#aed923",
-              padding: "20px",
-              width: "400px",
-            }}
-          >
-            <h1>Get Patient By ID</h1>
-          </Box>
-          <Box
-            sx={{
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-              borderRadius: "16px",
-              border: "1px solid black",
-              bgcolor: "#f2423f",
-              padding: "20px",
-              width: "400px",
-            }}
-          >
-            <h1>Delete Patient</h1>
-          </Box>
+          <Link to="/createPatient">
+            <Box
+              sx={{
+                ...boxStyle,
+                bgcolor: "#11a0d4",
+                "&:hover": {
+                  bgcolor: "#0f6381",
+                  transition: "0.5s",
+                  transform: "scale(1.1)",
+                },
+              }}
+            >
+              <h2>Create Patient</h2>
+            </Box>
+          </Link>
+          <Link to="/getPatient">
+            <Box
+              sx={{
+                ...boxStyle,
+                bgcolor: "#1ac94c",
+                "&:hover": {
+                  bgcolor: "#138a35",
+                  transition: "0.5s",
+                  transform: "scale(1.1)",
+                },
+              }}
+            >
+              <h2>Get All Patients</h2>
+            </Box>
+          </Link>
+          <Link to="/getPatientById">
+            <Box
+              sx={{
+                ...boxStyle,
+                bgcolor: "#aed923",
+                "&:hover": {
+                  bgcolor: "#779419",
+                  transition: "0.5s",
+                  transform: "scale(1.1)",
+                },
+              }}
+            >
+              <h2>Get Patient By ID</h2>
+            </Box>
+          </Link>
+          <Link to="/updatePatient">
+            <Box
+              sx={{
+                ...boxStyle,
+                bgcolor: "#4b2fc9",
+                "&:hover": {
+                  bgcolor: "#2b1881",
+                  transition: "0.5s",
+                  transform: "scale(1.1)",
+                },
+              }}
+            >
+              <h2>Update Patient Data</h2>
+            </Box>
+          </Link>
+          <Link to="/deletePatient">
+            <Box
+              sx={{
+                ...boxStyle,
+                bgcolor: "#f2423f",
+                "&:hover": {
+                  bgcolor: "#9c2b29",
+                  transition: "0.5s",
+                  transform: "scale(1.1)",
+                },
+              }}
+            >
+              <h2>Delete Patient</h2>
+            </Box>
+          </Link>
         </Box>
       </Container>
     </React.Fragment>
